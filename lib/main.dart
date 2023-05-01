7f9ef9974c01d630b9e6014c83ea00bace28725d
Zhasmina Aitmagambetova <aitmazh@gmail.com>
2023-05-01 17:15:04 +0600

    light theme added

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,45 @@ void main() {
   runApp(const MyApp());
 }
 
+MaterialColor myColor = MaterialColor(
+  const Color.fromRGBO(19, 41, 61, 1).value,
+  <int, Color>{
+    50: Color.fromRGBO(19, 41, 61, 0.1),
+    100: Color.fromRGBO(19, 41, 61, 0.2),
+    200: Color.fromRGBO(19, 41, 61, 0.3),
+    300: Color.fromRGBO(19, 41, 61, 0.4),
+    400: Color.fromRGBO(19, 41, 61, 0.5),
+    500: Color.fromRGBO(19, 41, 61, 0.6),
+    600: Color.fromRGBO(19, 41, 61, 0.7),
+    700: Color.fromRGBO(19, 41, 61, 0.8),
+    800: Color.fromRGBO(19, 41, 61, 0.9),
+    900: Color.fromRGBO(19, 41, 61, 1),
+  },
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Speak IT',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        // primaryColor: const Color.fromRGBO(19, 41, 61, 1),
+        scaffoldBackgroundColor: const Color.fromRGBO(232, 241, 242, 1),
+        primarySwatch: myColor,
+
+        fontFamily: 'San Serif',
+
+        /* textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          titleLarge: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ), */
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Fglutter Demo Home Pae'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
