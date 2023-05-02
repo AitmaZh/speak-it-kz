f277a0ebe2a3b58aa432111b044c94d8b9b5f1df
Zhasmina Aitmagambetova <aitmazh@gmail.com>
2023-05-02 21:20:25 +0600

    custom color init

---
+++ lib/assets/my_color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+MaterialColor myColor = MaterialColor(
+  const Color.fromRGBO(19, 41, 61, 1).value,
+  <int, Color>{
+    50: Color.fromRGBO(19, 41, 61, 0.1),
+    100: Color.fromRGBO(19, 41, 61, 0.2),
+    200: Color.fromRGBO(19, 41, 61, 0.3),
+    300: Color.fromRGBO(19, 41, 61, 0.4),
+    400: Color.fromRGBO(19, 41, 61, 0.5),
+    500: Color.fromRGBO(19, 41, 61, 0.6),
+    600: Color.fromRGBO(19, 41, 61, 0.7),
+    700: Color.fromRGBO(19, 41, 61, 0.8),
+    800: Color.fromRGBO(19, 41, 61, 0.9),
+    900: Color.fromRGBO(19, 41, 61, 1),
+  },
+);
